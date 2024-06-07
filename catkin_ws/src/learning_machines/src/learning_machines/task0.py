401d048633322d77bf25988dd20692774f2bd3f7
Mark Musa Mitrani <M.M.MitraniSabah@student.tudelft.nl>
2024-06-07 02:15:38 +0200

    Added plotting function to task 0, changed mac command to start coppelia sim.

--- catkin_ws/src/learning_machines/src/learning_machines/task0.py
+++ catkin_ws/src/learning_machines/src/learning_machines/task0.py
@@ -9,6 +9,9 @@ from robobo_interface import (
     HardwareRobobo,
 )
 
+import pandas as pd
+import matplotlib.pyplot as plt
+import time
 
 def do_task0(rob: IRobobo, duration=10000):
     '''
@@ -22,7 +25,14 @@ def do_task0(rob: IRobobo, duration=10000):
         rob.play_simulation()
 
     direction = 1  # 1 = go in front, -1 = go backwards, 0 = stop
+
+    # Lists to store data
+    timestamps = []
+    sensor_data_list = []
+    direction_list = []
+
     # loop over 0.5 sec (can be reduced)
+    start_time = time.time()
     while duration > 0:
         rob.reset_wheels()
         rob.move(100 * direction, 100 * direction, 500)
@@ -34,9 +44,48 @@ def do_task0(rob: IRobobo, duration=10000):
         rob.sleep(0.5)
         duration -= 1000
 
+        # Collect data
+        timestamps.append(time.time() - start_time)
+        sensor_data_list.append(sensor_data)
+        direction_list.append(direction)
+
     if isinstance(rob, SimulationRobobo):
         rob.stop_simulation()
 
+    # Plot results
+    plot_data(timestamps, sensor_data_list, direction_list)
+
+def plot_data(timestamps, sensor_data_list, direction_list):
+    # Convert collected data to DataFrame
+    data = {
+        'time': timestamps,
+        'sensor_data': sensor_data_list,
+        'direction': direction_list
+    }
+    df = pd.DataFrame(data)
+
+    # Plotting sensor data and direction against time
+    fig, ax1 = plt.subplots()
+
+    # Plotting sensor data (assuming we plot the sum of all sensor readings)
+    ax1.set_xlabel('Time (s)')
+    ax1.set_ylabel('Sensor Data', color='tab:blue')
+    ax1.plot(df['time'], df['sensor_data'].apply(sum), color='tab:blue', label='Sensor Data')
+    ax1.tick_params(axis='y', labelcolor='tab:blue')
+
+    # Creating a second y-axis for direction
+    ax2 = ax1.twinx()
+    ax2.set_ylabel('Direction', color='tab:red')
+    ax2.plot(df['time'], df['direction'], color='tab:red', label='Direction')
+    ax2.tick_params(axis='y', labelcolor='tab:red')
+
+    # Title and legend
+    plt.title('Sensor Data and Direction Over Time')
+    fig.tight_layout()
+    fig.legend(loc='upper left', bbox_to_anchor=(0.1,0.9))
+
+    # Show plot
+    plt.show()
 
 def react_to_sensors(sensor_data):
     """
--- scripts/start_coppelia_sim.zsh
+++ scripts/start_coppelia_sim.zsh
@@ -6,4 +6,4 @@
 ${1:?"Specify the scene you want to load as a first argument"}
 
 # Presumes you have CoppeliaSim extracted to ./coppeliaSim.app
-./coppeliaSim.app/Contents/MacOS/coppeliaSim "$1" $3 "-GzmqRemoteApi.rpcPort=${2:-23000}"
+sudo ./coppeliaSim.app/Contents/MacOS/coppeliaSim "$1" $3 "-GzmqRemoteApi.rpcPort=${2:-23000}"
