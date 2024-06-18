2b6964443e6f607c962495e3ecea20be18099b22
lucien150 <55390506+lucien150@users.noreply.github.com>
2024-06-18 12:04:47 +0200

    Update __init__.py

--- catkin_ws/src/learning_machines/src/learning_machines/__init__.py
+++ catkin_ws/src/learning_machines/src/learning_machines/__init__.py
@@ -1,5 +1,5 @@
 from .test_actions import run_all_actions
 from .task0 import do_task0
-from .task1 import run_task2
+from .task2 import run_task2
 
 __all__ = ("run_all_actions", "do_task0","run_task1")
