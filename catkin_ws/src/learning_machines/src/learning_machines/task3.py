1727b8f71ea6c1ce9aee424ad5366923f6821d6e
DanyPaisDaSilva <danypaisdasilva@gmail.com>
2024-06-26 19:16:43 +0200

    added pooling function

--- catkin_ws/src/learning_machines/src/learning_machines/task3.py
+++ catkin_ws/src/learning_machines/src/learning_machines/task3.py
@@ -39,6 +39,7 @@ def apply_mask(img, state="RED"):
         # apply green mask
         return cv2.inRange(img, (45, 70, 70), (85, 255, 255))
 
+
 def apply_red_mask(img):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
@@ -58,6 +59,7 @@ def apply_red_mask(img):
     mask = cv2.bitwise_or(mask1, mask2)
 
     return mask
+
 
 def set_resolution(img, resolution=64):
     # sim photo format is (512, 512)
@@ -96,6 +98,47 @@ def apply_morphology(image):
 
     # Perform the 'opening' operation, which is equivalent to erosion followed by dilation.
     return opened_image
+
+
+def pooling(mat, ksize, method='max', pad=False):
+    """Non-overlapping pooling on 2D or 3D data.
+    (ref: https://stackoverflow.com/questions/42463172/how-to-perform-max-mean-pooling-on-a-2d-array-using-numpy)
+
+    <mat>: ndarray, input array to pool.
+    <ksize>: tuple of 2, kernel size in (ky, kx).
+    <method>: str, 'max for max-pooling,
+                   'mean' for mean-pooling.
+    <pad>: bool, pad <mat> or not. If no pad, output has size
+           n//f, n being <mat> size, f being kernel size.
+           if pad, output has size ceil(n/f).
+
+    Return <result>: pooled matrix.
+    """
+
+    m, n = mat.shape[:2]
+    ky, kx = ksize
+
+    _ceil = lambda x, y: int(np.ceil(x / float(y)))
+
+    if pad:
+        ny = _ceil(m, ky)
+        nx = _ceil(n, kx)
+        size = (ny * ky, nx * kx) + mat.shape[2:]
+        mat_pad = np.full(size, np.nan)
+        mat_pad[:m, :n, ...] = mat
+    else:
+        ny = m // ky
+        nx = n // kx
+        mat_pad = mat[:ny * ky, :nx * kx, ...]
+
+    new_shape = (ny, ky, nx, kx) + mat.shape[2:]
+
+    if method == 'max':
+        result = np.nanmax(mat_pad.reshape(new_shape), axis=(1, 3))
+    else:
+        result = np.nanmean(mat_pad.reshape(new_shape), axis=(1, 3))
+
+    return result
 
 
 ####################
@@ -220,7 +263,7 @@ class RoboboEnv(gym.Env):
             self.robobo.play_simulation()
 
             # reset phone tilt & wheels
-            self.robobo.set_phone_tilt(95, 50)
+            self.robobo.set_phone_tilt(105, 50)
             self.robobo.reset_wheels()
 
             # TODO: check default position in arena_approach.ttt for
@@ -247,10 +290,10 @@ class RoboboEnv(gym.Env):
         reward = get_reward(image_masked, action, self.state)
 
         if reward > 0:
-            image_unmasked = set_resolution(self.robobo.get_image_front())
-
+            image1 = self.robobo.get_image_front()
+            image2 = cv2.cvtColor(apply_red_mask(self.get_image()) * 255, cv2.COLOR_GRAY2BGR)
             # Concatenate the images horizontally
-            combined_image = cv2.hconcat([cv2.cvtColor(image_masked* 255, cv2.COLOR_GRAY2BGR), image_unmasked])
+            combined_image = cv2.hconcat([image1, image2])
 
             cv2.imwrite(str(FIGURES_DIR / f"test_img_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.jpeg"),
                         combined_image)
@@ -266,7 +309,7 @@ class RoboboEnv(gym.Env):
             done = True
             print("Food collected!")
         # if robot stuck / too much time passed (480s = 8 min) --> restart
-        if isinstance(self.robobo, SimulationRobobo) and self.robobo.get_sim_time() > 60: #480
+        if isinstance(self.robobo, SimulationRobobo) and self.robobo.get_sim_time() > 60:  # 480
             done = True
             print("Ran out of time :(")
 
